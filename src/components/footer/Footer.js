d14771ae8d89578c5beffd058d0a92393fd9b8ef
abiodunraheem <raheemabiodun23@gmail.com>
2022-12-02 01:50:34 +0100

    Add styles to testimonials and footer components

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './footer.css';
+import { SlSocialFacebook } from 'react-icons/sl';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { FiTwitter } from 'react-icons/fi';
 
 const Footer = () => (
   <footer>
     <a href="#" className="footer_logo">Shammer</a>
-    <ul className="footer_link">
+    <ul className="footer_links">
       <li><a href="#">Home</a></li>
       <li><a href="#about">About</a></li>
       <li><a href="#experience">Experience</a></li>
@@ -14,10 +17,12 @@ const Footer = () => (
       <li><a href="#contact">Contact</a></li>
     </ul>
     <div className="footer_socials">
-      <a href="https://facebook.com"></a>
-      <a href="https://instagram.com"></a>
-      <a href="https://twitter.com"></a>
-      <a href="https://facebook.com"></a>
+      <a href="https://facebook.com" aria-label="facebook"><SlSocialFacebook /></a>
+      <a href="https://instagram.com" aria-label="instagram"><AiOutlineInstagram /></a>
+      <a href="https://twitter.com" aria-label="twitter"><FiTwitter /></a>
+    </div>
+    <div className="footer_copyright">
+      <small className="">&copy; Raheem Abiodun. All right reserved.</small>
     </div>
   </footer>
 );
